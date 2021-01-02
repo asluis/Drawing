a361e816b9249aa50254a6b19d305ac5a4d149d2
asluis <asluisinfo@gmail.com>
2021-01-02 14:36:58 -0800

    Added code for varius shapes with several techniques.

--- ContentView.swift
+++ ContentView.swift
@@ -9,8 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Triangle()
+            .fill(Color.blue)
+            .frame(width: 300, height: 300)
+        Arc(startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
+            .frame(width: 300, height: 300)
+        //Circle()
+            //.stroke(Color.blue, lineWidth: 40) Border goes outside of view
+        Circle()
+            .strokeBorder(Color.blue, lineWidth: 40)
+        
+    
+    }
+    
+    
+}
+
+
+struct Triangle: Shape { // Creates a triangle of any size based on rectangle
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+    
+// InsettableShape IS a shape, no need to implement both
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount:CGFloat = 0
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90) // needed to make angles more intuitive in swiftUI
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
