6c4cfa7ad8349686362f265a32c4ca5601dd5322
asluis <asluisinfo@gmail.com>
2021-01-02 14:53:22 -0800

    Created a star function.

--- ContentView.swift
+++ ContentView.swift
@@ -20,6 +20,11 @@ struct ContentView: View {
         Circle()
             .strokeBorder(Color.blue, lineWidth: 40)
         
+        Star()
+            //.fill(Color.blue)
+            .stroke(Color.blue, lineWidth: 5)
+            .frame(width: 300, height: 300)
+            
     
     }
     
@@ -65,6 +70,35 @@ struct Arc: InsettableShape {
         return arc
     }
 }
+
+
+// Personal Challenge:
+//  Draw a star
+
+
+struct Star:Shape{
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY - rect.midY / 2))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY - rect.midY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY - rect.midY / 2))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - rect.midY / 2))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - rect.midY / 2))
+        
+        
+        
+        
+        
+        return path
+    }
+    
+    
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
