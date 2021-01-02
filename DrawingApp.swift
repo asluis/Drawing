227c98a6ce45d3120027d9d531b11722ce02a9fb
asluis <asluisinfo@gmail.com>
2021-01-01 20:07:08 -0800

    initial code upload

---
+++ DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Luis Alvarez on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
