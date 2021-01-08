bc896b712d02c8608c535841268d731586be7651
asluis <asluisinfo@gmail.com>
2021-01-07 19:41:34 -0800

    Small change

--- ContentView.swift
+++ ContentView.swift
@@ -73,7 +73,6 @@ struct Arrow: Shape{
     
     func path(in rect: CGRect) -> Path {
         var path = Path();
-        
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.midX / 2 + rect.midX/4, y: rect.midY))
